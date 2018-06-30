06244d5427651e0d8aa69348fb41744c6649ae73
kentatogashi <4276550+kentatogashi@users.noreply.github.com>
2018-06-30 21:48:56 +0900

    Add sell and buy function.

--- trade.py
+++ trade.py
@@ -1,25 +1,47 @@
+import configparser as cp
 import os
 import sys
 import ccxt
+import json
 from datetime import datetime, timedelta, timezone
 
+class LimitOrderError(Exception):
+    pass
+
 class Trade:
-    def __init__(self, conf=None):
-        if conf:
-            self.ticker = conf.ticker
-            self.exchange = eval("ccxt.%s()" % conf.exchange)
-            self.exchange.apiKey = conf.API_KEY
-            self.exchange.secret = conf.SECRET
+    def __init__(self, conf_file=None):
+        conf = cp.ConfigParser()
+        conf.read(conf_file, 'UTF-8')
+        self.ticker = conf.get('setting', 'ticker')
+        self.exchange = eval("ccxt.%s()" % conf.get('setting', 'exchange'))
+        self.exchange.apiKey = conf.get('setting', 'api_key')
+        self.exchange.secret = conf.get('setting', 'secret')
+        self.open_orders_limit = 1
+
+    def order_is_enable(self):
+        open_orders = self.exchange.fetch_open_orders(symbol=self.ticker)
+        if self.open_orders_limit > len(open_orders):
+            return True
+        return False
+
+    def buy(self, amount, price, _type='limit'):
+        if self.order_is_enable():
+            result = self.exchange.create_order(
+                    symbol=self.ticker, type=_type, 
+                    side='buy', amount=amount, price=price)
+            return json.dumps(result, indent=True)
         else:
-            if os.environ.get('API_KEY'):
-                self.exchange.apiKey = os.environ.get('API_KEY')
-            else:
-                sys.exit('not found API_KEY')
-            if os.environ.get('SECRET'):
-                self.exchange.secret = os.environ.get('SECRET')
-            else:
-                sys.exit('not found SECRET')
-                
+            raise LimitOrderError
+
+    def sell(self, amount, price, _type='limit'):
+        if self.order_is_enable():
+            result = self.exchange.create_order(
+                    symbol=self.ticker, type=_type, 
+                    side='sell', amount=amount, price=price)
+            return json.dumps(result, indent=True)
+        else:
+            raise LimitOrderError
+
     def get_symbols(self):
         return self.exchange.symbols
 
@@ -36,12 +58,6 @@ class Trade:
                 limit=100)
         return historical_data
 
-    def buy(self, amount, price):
-        raise NotImplementedError
-
-    def sell(self, amount, price):
-        raise NotImplementedError
-
     def checkOrder(self):
         raise NotImplementedError
 
@@ -52,13 +68,13 @@ class Trade:
         result = self.exchange.fetch_markets()
         print(json.dumps(result, indent=True))
         print('fetch_ticker')
-        result = self.exchange.fetch_ticker('BTC/JPY')
+        result = self.exchange.fetch_ticker(self.ticker)
         print(json.dumps(result, indent=True))
         print('fetch_order_book')
-        result = self.exchange.fetch_order_book(symbol='BTC/JPY')
+        result = self.exchange.fetch_order_book(symbol=self.ticker)
         print(result)
         print('fetch_trades')
-        result = self.exchange.fetch_trades(symbol='BTC/JPY', limit=2)
+        result = self.exchange.fetch_trades(symbol=self.ticker, limit=2)
         print(result)
 
     def private_api_example(self):
@@ -66,4 +82,7 @@ class Trade:
         print(json.dumps(result, indent=True))
 
 if __name__ == '__main__':
-    trade = Trade()
+    conf_file = 'trade.conf'
+    trade = Trade(conf_file)
+    print(trade.buy(amount=0.0001, price=100000))
+    print(trade.sell(amount=0.0001, price=10000000))
